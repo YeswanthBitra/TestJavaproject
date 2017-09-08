29f416ca210770d7b469a08c708e7b2b23ec50e6
Training_h2a.09.19 <Training_h2a.09.19@HDC2-D-4XVD8R1.dir.svc.accenture.com>
2017-09-08 12:34:24 +0530

    commit4

--- src/main/Java/TestServlet.java
+++ src/main/Java/TestServlet.java
@@ -25,7 +25,7 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().write("This is Test project Haiiii 123");
+		response.getWriter().write("This");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
